49acb66e56d739ddb49496c5fcf83d3794ae109a
hieuchien18 <59491173+hieuchien18@users.noreply.github.com>
2024-10-27 23:49:44 +0700

    Update issued-books.php

--- issued-books.php
+++ issued-books.php
@@ -1,139 +1,147 @@
 <?php
 session_start();
-error_reporting(0);
+error_reporting(E_ALL); // Change to E_ALL for debugging
+ini_set('display_errors', 1); // Enable error display
+
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-    {   
-header('location:index.php');
-}
-else{ 
-if(isset($_GET['del']))
-{
-$id=$_GET['del'];
-$sql = "delete from tblbooks  WHERE id=:id";
-$query = $dbh->prepare($sql);
-$query -> bindParam(':id',$id, PDO::PARAM_STR);
-$query -> execute();
-$_SESSION['delmsg']="Category deleted scuccessfully ";
-header('location:manage-books.php');
 
-}
-
+if (strlen($_SESSION['login']) == 0) {
+    header('location:index.php');
+    exit; // Always use exit after a header redirect
+} else {
+    // Handle deletion of a book
+    if (isset($_GET['del'])) {
+        $id = intval($_GET['del']); // Ensure it's an integer
+        $sql = "DELETE FROM tblbooks WHERE id = :id";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':id', $id, PDO::PARAM_INT);
+        if ($query->execute()) {
+            $_SESSION['delmsg'] = "Book deleted successfully.";
+        } else {
+            $_SESSION['delmsg'] = "Error deleting book.";
+        }
+        header('location:manage-books.php');
+        exit;
+    }
 
     ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Online Library Management System | Manage Issued Books</title>
-    <!-- BOOTSTRAP CORE STYLE  -->
+    <!-- BOOTSTRAP CORE STYLE -->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
-    <!-- FONT AWESOME STYLE  -->
+    <!-- FONT AWESOME STYLE -->
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
-    <!-- DATATABLE STYLE  -->
+    <!-- DATATABLE STYLE -->
     <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
-    <!-- CUSTOM STYLE  -->
+    <!-- CUSTOM STYLE -->
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
 </head>
-<body>
-      <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-    <div class="content-wrapper">
-         <div class="container">
-        <div class="row pad-botm">
-            <div class="col-md-12">
-                <h4 class="header-line">Manage Issued Books</h4>
-    </div>
-    
 
-            <div class="row">
+<body>
+    <!------MENU SECTION START-->
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row pad-botm">
                 <div class="col-md-12">
-                    <!-- Advanced Tables -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                          Issued Books 
-                        </div>
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Book Name</th>
-                                            <th>ISBN </th>
-                                            <th>Issued Date</th>
-                                            <th>Return Date</th>
-                                            <th>Fine in(USD)</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-<?php 
-$sid=$_SESSION['stdid'];
-$sql="SELECT Server=tcp:qltv1.database.windows.net,1433;Initial Catalog=qltv1;Persist Security Info=False;User ID=admin123;Password=Admin123@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
-$query = $dbh -> prepare($sql);
-$query-> bindParam(':sid', $sid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{               ?>                                      
-                                        <tr class="odd gradeX">
-                                            <td class="center"><?php echo htmlentities($cnt);?></td>
-                                            <td class="center"><?php echo htmlentities($result->BookName);?></td>
-                                            <td class="center"><?php echo htmlentities($result->ISBNNumber);?></td>
-                                            <td class="center"><?php echo htmlentities($result->IssuesDate);?></td>
-                                            <td class="center"><?php if($result->ReturnDate=="")
-                                            {?>
-                                            <span style="color:red">
-                                             <?php   echo htmlentities("Not Return Yet"); ?>
-                                                </span>
-                                            <?php } else {
-                                            echo htmlentities($result->ReturnDate);
-                                        }
-                                            ?></td>
-                                              <td class="center"><?php echo htmlentities($result->fine);?></td>
-                                         
-                                        </tr>
- <?php $cnt=$cnt+1;}} ?>                                      
-                                    </tbody>
-                                </table>
+                    <h4 class="header-line">Manage Issued Books</h4>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- Advanced Tables -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Issued Books
                             </div>
-                            
+                            <div class="panel-body">
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Book Name</th>
+                                                <th>ISBN</th>
+                                                <th>Issued Date</th>
+                                                <th>Return Date</th>
+                                                <th>Fine (USD)</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php 
+                                            $sid = $_SESSION['stdid'];
+                                            $sql = "SELECT * FROM tblissuedbookdetails WHERE StudentID = :sid";
+                                            $query = $dbh->prepare($sql);
+                                            $query->bindParam(':sid', $sid, PDO::PARAM_STR);
+                                            $query->execute();
+                                            $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                            $cnt = 1;
+                                            if ($query->rowCount() > 0) {
+                                                foreach ($results as $result) { ?>                                      
+                                            <tr class="odd gradeX">
+                                                <td class="center"><?php echo htmlentities($cnt); ?></td>
+                                                <td class="center"><?php echo htmlentities($result->BookName); ?></td>
+                                                <td class="center"><?php echo htmlentities($result->ISBNNumber); ?></td>
+                                                <td class="center"><?php echo htmlentities($result->IssuesDate); ?></td>
+                                                <td class="center"><?php 
+                                                    if ($result->ReturnDate == "") { ?>
+                                                        <span style="color:red">Not Returned Yet</span>
+                                                    <?php } else {
+                                                        echo htmlentities($result->ReturnDate);
+                                                    } ?>
+                                                </td>
+                                                <td class="center"><?php echo htmlentities($result->fine); ?></td>
+                                                <td class="center">
+                                                    <a href="manage-books.php?del=<?php echo htmlentities($result->id); ?>" onclick="return confirm('Are you sure you want to delete this book?');" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                            <?php $cnt = $cnt + 1; 
+                                                }
+                                            } else {
+                                                echo "<tr><td colspan='7' class='text-center'>No issued books found.</td></tr>";
+                                            } ?>                                      	
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <?php if (isset($_SESSION['delmsg'])) { ?>
+                                    <div class="alert alert-success">
+                                        <?php echo htmlentities($_SESSION['delmsg']); ?>
+                                        <?php unset($_SESSION['delmsg']); // Clear message after displaying ?>
+                                    </div>
+                                <?php } ?>
+                            </div>
                         </div>
+                        <!--End Advanced Tables -->
                     </div>
-                    <!--End Advanced Tables -->
                 </div>
             </div>
-
-
-            
-    </div>
-    </div>
+        </div>
     </div>
 
-     <!-- CONTENT-WRAPPER SECTION END-->
-  <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
-    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
-    <!-- CORE JQUERY  -->
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php'); ?>
+    <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME -->
+    <!-- CORE JQUERY -->
     <script src="assets/js/jquery-1.10.2.js"></script>
-    <!-- BOOTSTRAP SCRIPTS  -->
+    <!-- BOOTSTRAP SCRIPTS -->
     <script src="assets/js/bootstrap.js"></script>
-    <!-- DATATABLE SCRIPTS  -->
+    <!-- DATATABLE SCRIPTS -->
     <script src="assets/js/dataTables/jquery.dataTables.js"></script>
     <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
+    <!-- CUSTOM SCRIPTS -->
     <script src="assets/js/custom.js"></script>
-
 </body>
 </html>
 <?php } ?>
